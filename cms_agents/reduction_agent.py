195da294c4ae073f304f00d8039132261bd4cb37
Thomas A Caswell <tcaswell@bnl.gov>
2023-02-08 10:37:55 -0500

    WIP: pass at getting event model to work

--- cms_agents/reduction_agent.py
+++ cms_agents/reduction_agent.py
@@ -1,24 +1,67 @@
 import datetime
 import pprint
 import uuid
+import time as ttime
 
 from bluesky_kafka import Publisher, RemoteDispatcher
 import nslsii.kafka_utils
+
+from ophyd.utils import data_shape, data_type
+from event_model import compose_run
 
 from tiled.client import from_profile
 
 
 def reduce_run(bluesky_run):
+    """
+    Reduce data from a single bluesky run.
+
+    Parameters
+    ----------
+    bluesky_run : BlueskyRun
+        The run to be reduced, assumed to be a v2 run object.
+
+    Returns
+    -------
+    reduced : dict
+        A single event
+
+    metadata : dict
+        Will be top-level in the reduced start document
+    """
     print(f"give reduced on run {bluesky_run}")
-    reduced = {
-        "next big thing": "avocado toast"
-    }
+    reduced = {"next big thing": "avocado toast"}
 
-    return reduced
+    return reduced, {"raw_start": bluesky_run.metadata["start"]}
 
 
-def publish_reduced_documents(reduced, reduced_publisher):
+def publish_reduced_documents(reduced, metadata, reduced_publisher):
+    cr = compose_run(metadata=metadata)
+    reduced_publisher("start", cr.start_doc)
 
+    desc_bundle = cr.compose_descriptor(
+        name="primary",
+        data_keys={
+            k: {
+                "dtype": data_type(v),
+                "shape": data_shape(v),
+                "source": "computed",
+            }
+            for k, v in reduced.items()
+        },
+    )
+
+    reduced_publisher("descriptor", desc_bundle.descriptor_doc)
+    t = ttime.time()
+    reduced_publisher(
+        "event",
+        desc_bundle.compose_event(
+            data=reduced,
+            timestamps={k: t for k in reduced},
+        ),
+    )
+
+    reduced_publisher("stop", cr.compose_stop())
 
 
 def respond_to_stop_with_reduced():
@@ -28,39 +71,35 @@ def respond_to_stop_with_reduced():
     cms_tiled_client = from_profile("cms")
 
     reduced_publisher = Publisher(
-        topic="",
+        topic="cms.reduced.bluesky_documents",
         bootstrap_servers=",".join(kafka_config["bootstrap_servers"]),
-        producer_config=kafka_config["producer_config"]
+        producer_config=kafka_config["producer_config"],
     )
 
     def on_stop_reduce_run(name, doc):
-        print(
-            f"{datetime.datetime.now().isoformat()} document: {name}\n"
-            f"contents: {pprint.pformat(doc)}\n"
-        )
+        print(f"{datetime.datetime.now().isoformat()} document: {name}\n" f"contents: {pprint.pformat(doc)}\n")
         if name == "stop":
             # look up the results of this run
             run_start_id = doc["run_start"]
             print(f"found run_start id {run_start_id}")
             bluesky_run = cms_tiled_client[run_start_id]
             reduced = reduce_run(bluesky_run)
-            publish_documents(reduced, reduced_publisher)
+            publish_reduced_documents(reduced, reduced_publisher)
         else:
             pass
 
-
     # this consumer should not be in a group with other consumers
     #   so generate a unique consumer group id for it
-    unique_group_id = f"advise-{str(uuid.uuid4())[:8]}"
+    unique_group_id = f"reduce-{str(uuid.uuid4())[:8]}"
 
     kafka_dispatcher = RemoteDispatcher(
-        topics=[f"cms.bluesky.runengine.documents"],
+        topics=["cms.bluesky.runengine.documents"],
         bootstrap_servers=",".join(kafka_config["bootstrap_servers"]),
         group_id=unique_group_id,
         consumer_config=kafka_config["runengine_producer_config"],
     )
 
-    kafka_dispatcher.subscribe(print_message)
+    kafka_dispatcher.subscribe(on_stop_reduce_run)
     kafka_dispatcher.start()
 
 
