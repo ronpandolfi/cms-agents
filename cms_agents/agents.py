0816ca68339ba893894f5633ecaefaa9e771a9fc
maffettone <phillip.maffettone@gmail.com>
2023-02-28 16:48:55 -0500

    API: Update PTA agents with new API

--- cms_agents/agents.py
+++ cms_agents/agents.py
@@ -1,34 +1,69 @@
 import uuid
-from typing import Sequence, Tuple, Union
+from typing import List, Sequence, Tuple, Union
 
 import nslsii.kafka_utils
+import tiled
+from bluesky_adaptive.agents.base import AgentConsumer
 from bluesky_adaptive.agents.simple import SequentialAgentBase
-from numpy.typing import ArrayLike
+from bluesky_kafka import Publisher
 from bluesky_queueserver_api.zmq import REManagerAPI
+from numpy.typing import ArrayLike
 
 
 class CMSBaseAgent:
-    measurement_plan_name = ...
+    def measurement_plan(self, point: ArrayLike) -> Tuple[str, List, dict]:
+        """Default measurement plan is a count on the pilatus, for a given num
 
-    @staticmethod
-    def measurement_plan_args(point) -> list:
-        """
-        List of arguments to pass to plan from a point to measure.
-        This is a good place to transform relative into absolute motor coords.
-        """
-        return [["pilatus2M"]]
+        Parameters
+        ----------
+        point : ArrayLike
+            Next point to measure using a given plan
 
-    @staticmethod
-    def measurement_plan_kwargs(point) -> dict:
+        Returns
+        -------
+        plan_name : str
+        plan_args : List
+            List of arguments to pass to plan from a point to measure.
+        plan_kwargs : dict
+            Dictionary of keyword arguments to pass the plan, from a point to measure.
         """
-        Construct dictionary of keyword arguments to pass the plan, from a point to measure.
-        This is a good place to transform relative into absolute motor coords.
-        """
-        return dict(num=point)
+        return "count", [["pilatus2M"]], dict(num=point)
 
     def unpack_run(self, run) -> Tuple[Union[float, ArrayLike], Union[float, ArrayLike]]:
         print(run)
         return 0, 0
+
+    @staticmethod
+    def get_beamline_objects() -> dict:
+        beamline_tla = "cms"
+        kafka_config = nslsii.kafka_utils._read_bluesky_kafka_config_file(
+            config_file_path="/etc/bluesky/kafka.yml"
+        )
+        qs = REManagerAPI(zmq_control_addr="tcp://xf11bm-ws1.nsls2.bnl.local:60615")
+
+        kafka_consumer = AgentConsumer(
+            topics=[
+                f"{beamline_tla}.bluesky.reduced.documents",
+            ],
+            consumer_config=kafka_config["runengine_producer_config"],
+            bootstrap_servers=kafka_config["bootstrap_servers"],
+            group_id=f"echo-{beamline_tla}-{str(uuid.uuid4())[:8]}",
+        )
+
+        kafka_producer = Publisher(
+            topic=f"{beamline_tla}.bluesky.adjudicators",
+            bootstrap_servers=kafka_config["bootstrap_servers"],
+            key="cms.key",
+            producer_config=kafka_config["runengine_producer_config"],
+        )
+
+        return dict(
+            kafka_consumer=kafka_consumer,
+            kafka_producer=kafka_producer,
+            tiled_data_node=tiled.client.from_profile(f"{beamline_tla}"),
+            tiled_agent_node=tiled.client.from_profile(f"{beamline_tla}_bluesky_sandbox"),
+            qserver=qs,
+        )
 
     @staticmethod
     def get_beamline_kwargs() -> dict:
@@ -36,11 +71,11 @@ class CMSBaseAgent:
         kafka_config = nslsii.kafka_utils._read_bluesky_kafka_config_file(
             config_file_path="/etc/bluesky/kafka.yml"
         )
-        qs = REManagerAPI(zmq_control_addr='tcp://xf11bm-ws1.nsls2.bnl.local:60615')
+        qs = REManagerAPI(zmq_control_addr="tcp://xf11bm-ws1.nsls2.bnl.local:60615")
 
         return dict(
             kafka_group_id=f"echo-{beamline_tla}-{str(uuid.uuid4())[:8]}",
-            kafka_bootstrap_servers=','.join(kafka_config["bootstrap_servers"]),
+            kafka_bootstrap_servers=",".join(kafka_config["bootstrap_servers"]),
             kafka_consumer_config=kafka_config["runengine_producer_config"],
             kafka_producer_config=kafka_config["runengine_producer_config"],
             publisher_topic=f"{beamline_tla}.bluesky.adjudicators",
@@ -54,8 +89,6 @@ class CMSBaseAgent:
 
 
 class CMSSequentialAgent(CMSBaseAgent, SequentialAgentBase):
-    measurement_plan_name = 'count'
-
     def __init__(
         self,
         *,
@@ -63,6 +96,6 @@ class CMSSequentialAgent(CMSBaseAgent, SequentialAgentBase):
         relative_bounds: Tuple[Union[float, ArrayLike]] = None,
         **kwargs,
     ) -> None:
-        _default_kwargs = self.get_beamline_kwargs()
+        _default_kwargs = self.get_beamline_objects()
         _default_kwargs.update(kwargs)
         super().__init__(sequence=sequence, relative_bounds=relative_bounds, **_default_kwargs)
