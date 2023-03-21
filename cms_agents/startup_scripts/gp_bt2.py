cc44beaaa586274a637a30322e6b45efd114b3cb
maffettone <phillip.maffettone@gmail.com>
2023-03-21 16:02:54 -0400

    ENH: trial scripts

---
+++ cms_agents/startup_scripts/gp_bt2.py
@@ -0,0 +1,34 @@
+from typing import List, Tuple
+
+from bluesky_adaptive.server import shutdown_decorator, startup_decorator
+from numpy.typing import ArrayLike
+
+from cms_agents.agents import CMSSingleTaskAgent
+
+
+class SingleTaskAgent(CMSSingleTaskAgent):
+    def __init__(
+        self,
+        *,
+        bounds: ArrayLike,
+        independent_key: str = "metadata_extract__x_position",
+        target_key: str = "value",
+        **kwargs
+    ):
+        super().__init__(bounds=bounds, independent_key=independent_key, target_key=target_key, **kwargs)
+
+    def measurement_plan(self, point: ArrayLike) -> Tuple[str, List, dict]:
+        return "agent_feeback_plan", [point], dict()
+
+
+agent = SingleTaskAgent([0.0, 50.0], report_on_tell=False, ask_on_tell=False)
+
+
+@startup_decorator
+def startup():
+    agent.start()
+
+
+@shutdown_decorator
+def shutdown_agent():
+    return agent.stop()
