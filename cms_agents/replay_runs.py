c5b7ccab48f3d86fbd6fe634dbdf4b689d8c9f71
jklynch <jlynch@bnl.gov>
2023-05-19 08:31:57 -0400

    Add new features Added option to replay runs from the CMS tiled sandbox. Added shortcut to replay the previous list of runs.

--- cms_agents/replay_runs.py
+++ cms_agents/replay_runs.py
@@ -2,7 +2,7 @@ import argparse
 
 from bluesky_kafka import Publisher
 from nslsii.kafka_utils import _read_bluesky_kafka_config_file
-from tiled.client import from_profile
+from tiled.client import from_profile, from_uri
 
 
 def get_args():
@@ -14,10 +14,16 @@ def get_args():
         action="store_true", help="set to produce Kafka messsages"
     )
 
+    parser.add_argument(
+        "--replay-reduced-runs",
+        default=False,
+        action="store_true", help="set to replay runs from the CMS tiled sandbox"
+    )
+
     return parser.parse_args()
 
 
-def replay_runs(produce):
+def replay_runs(produce, replay_reduced_runs):
     kafka_config = _read_bluesky_kafka_config_file("/etc/bluesky/kafka.yml")
     bluesky_document_producer = Publisher(
         topic="cms.test",
@@ -26,17 +32,25 @@ def replay_runs(produce):
         producer_config=kafka_config["runengine_producer_config"],
     )
 
-    cms_client = from_profile("cms")
-
     print()
     if produce:
         print("replaying runs to topic cms.test")
     else:
         print("Kafka messages will not be produced because --produce was not specified on the command line")
+    
     print()
+    if replay_reduced_runs:
+        print("replaying reduced runs from the CMS tiled sandbox")
+        cms_client = from_uri("https://tiled.nsls2.bnl.gov/api/v1/node/metadata/cms/bluesky_sandbox")
+    else:
+        print("replaying CMS RunEngine runs")
+        cms_client = from_profile("cms")
+
     print("enter lists of space-separated scan ids to be replayed")
     print("enter an empty list to quit")
+
     print()
+    last_scan_ids = []
     while True:
         try:
             scan_ids_input = input("scan ids to replay: ")
@@ -45,6 +59,10 @@ def replay_runs(produce):
                 print("all done!")
                 break
             else:
+                if scan_ids == ".":
+                    print(f"replaying '{last_scan_ids}'")
+                    scan_ids = last_scan_ids
+
                 for scan_id in scan_ids:
                     print(f"replaying scan id {scan_id}")
                     if produce:
@@ -54,6 +72,9 @@ def replay_runs(produce):
                         bluesky_document_producer.flush()
                     else:
                         print("  no messages produced")
+                
+                last_scan_ids = scan_ids
+
         except KeyboardInterrupt:
             print()
             print("all done!")
